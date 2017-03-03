d389bb419fc2e33334475850dc2a3f590e3830df
Maxim Salnikov <salnikov@gmail.com>
2017-03-03 23:48:35 +0100

    logging added

--- index.js
+++ index.js
@@ -1,26 +1,49 @@
+// Reading process.env variables from .env file
 require('dotenv').config()
 
+// Scaffolding Express app
 var express = require('express')
 var app = express()
 var server = require('http').createServer(app)
-
-var base64 = require('base-64')
-
-var CronJob = require('cron').CronJob
-
-var cors = require('cors')
-app.use(cors())
-app.options('*', cors())
 
 var bodyParser = require('body-parser')
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 
-const webPush = require('web-push')
+// Enabling CORS
+var cors = require('cors')
+app.use(cors())
+app.options('*', cors())
+
+// Setting up detailed logging
+var winston = require('winston')
+var logger = new winston.Logger({
+  transports: [
+    new winston.transports.Console({
+      handleExceptions: true,
+      json: true
+    })
+  ],
+  exitOnError: false
+})
+logger.stream = {
+  write: function (message, encoding) {
+    logger.info(message)
+  }
+}
+app.use(require('morgan')('combined', { 'stream': logger.stream }))
+
+// Setting Web Push credentials
+var webPush = require('web-push')
+webPush.setVapidDetails(
+  'mailto:salnikov@gmail.com',
+  process.env.VAPID_PUBLIC_KEY,
+  process.env.VAPID_PRIVATE_KEY
+)
 var pushSubscription
 
+// Connecting to Twitter
 var Twitter = require('twitter')
-
 var twitterClient = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
@@ -28,68 +51,109 @@ var twitterClient = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-/**
- * Grab a list of favorited tweets
- **/
-twitterClient.get('favorites/list', function (error, tweets, response) {
-  if (!error) {
-    // console.log(tweets)
-  }
-})
-
+// Listening to tweets stream
 twitterClient.stream('statuses/filter', {track: 'angular'}, function (stream) {
   stream.on('data', function (tweet) {
-    console.log(JSON.stringify(tweet, null, 2))
+
+    // https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification
+    var notificationData = {}
+    notificationData.notification = {
+      actions: [{
+        action: 'retweet',
+        title: 'Retweet'
+      }, {
+        action: 'reply',
+        title: 'Reply'
+      }],
+      body: [ tweet.user.name, tweet.text ].join(': '),
+      dir: 'auto',
+      icon: tweet.profile_image_url_https,
+      lang: tweet.lang,
+      renotify: true,
+      requireInteraction: true,
+      tag: tweet.id,
+      vibrate: [300, 100, 400],
+      data: tweet.timestamp_ms
+    }
+
+    sendNotification(JSON.stringify(notificationData))
+    logger.info(notificationData)
   })
 })
 
-webPush.setVapidDetails(
-  'mailto:salnikov@gmail.com',
-  process.env.VAPID_PUBLIC_KEY,
-  process.env.VAPID_PRIVATE_KEY
-)
+// Subscribe to Web Push
+app.get('/feed', function (req, res, next) {
+  twitterClient.get('favorites/list', function (error, tweets, response) {
+    if (!error) {
+      res.send(tweets)
+    } else {
+      logger.error(error)
+      throw new Error('Error receiving tweets')
+    }
+  })
+})
 
-app.post('/subscription', function (req, res, next) {
+// Subscribe to Web Push
+app.post('/webpush', function (req, res, next) {
   if (req.body.action === 'subscribe') {
     pushSubscription = req.body.subscription
 
-    console.log('Subscription registered: ')
-    console.log(JSON.stringify(pushSubscription, null, 2))
+    logger.info('Subscription registered: ', pushSubscription)
+
+    res.send({
+      text: 'Subscribed',
+      status: '200'
+    })
   } else {
     throw new Error('Unsupported action')
   }
-
-  res.send({
-    text: 'Subscribed',
-    status: '200'
-  })
 })
 
 function sendNotification (payload) {
   if (pushSubscription) {
     webPush.sendNotification(pushSubscription, payload)
       .then(function (response) {
-        console.log('Push sent')
-      // console.log(JSON.stringify(response, null, 2))
+        logger.info('Push sent', payload, response)
       })
-      .catch(function (err) {
-        console.error('Push error: ' + err)
+      .catch(function (error) {
+        logger.error('Push error: ', error)
       })
   }
 }
 
-function sendToBrowser (messageContent) {
-  sendNotification(JSON.stringify(messageContent))
-}
+// Starting Express
 
-// Starting server
-
-server.listen(process.env.PORT || 3001, function () {
-  console.log('Listening on port ' + (process.env.PORT || 3001))
+server.listen(process.env.PORT || 3000, function () {
+  logger.info('Listening on port ' + (process.env.PORT || 3000))
 })
 
-// Starting push
+// Backup option - Web Push by timer
+
+var CronJob = require('cron').CronJob
 
 new CronJob('*/5 * * * * *', function () {
-  // console.log('You will see this message every second')
-}, null, true)
+
+  var notificationData = {}
+  notificationData.notification = {
+    actions: [{
+      action: 'retweet',
+      title: 'Retweet'
+    }, {
+      action: 'reply',
+      title: 'Reply'
+    }],
+    body: 'My notification',
+    dir: 'auto',
+    icon: 'https://image',
+    lang: 'en',
+    renotify: true,
+    requireInteraction: true,
+    tag: 'Some ID',
+    vibrate: [300, 100, 400],
+    data: 'Test data'
+  }
+
+  sendNotification(JSON.stringify(notificationData))
+  logger.info(notificationData)
+
+}, null, false) //set the last parameter to true to start CronJob
