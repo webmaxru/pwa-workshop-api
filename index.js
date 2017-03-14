eabada2140769356246bef2aca27dd3f56b9db92
Maxim Salnikov <salnikov@gmail.com>
2017-03-14 12:27:18 +0100

    multiple push subscriptions are available

--- index.js
+++ index.js
@@ -50,7 +50,7 @@ webPush.setVapidDetails(
   process.env.VAPID_PUBLIC_KEY,
   process.env.VAPID_PRIVATE_KEY
 )
-var pushSubscription
+var pushSubscriptions = []
 
 // Connecting to Twitter
 // Get your credentials here: https://apps.twitter.com/app/new
@@ -62,7 +62,45 @@ var twitterClient = new Twitter({
   access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 })
 
-// Listening to tweets stream
+// Subscribe to Web Push
+app.post('/webpush', function (req, res, next) {
+  logger.info('Web push subscription object received: ', req.body.subscription)
+
+  if (req.body.action === 'subscribe') {
+    if (arrayObjectIndexOf(pushSubscriptions, req.body.subscription.endpoint, 'endpoint') == -1) {
+      pushSubscriptions.push(req.body.subscription)
+      logger.info('Subscription registered: ' + req.body.subscription.endpoint)
+    } else {
+      logger.info('Subscription was already registered: ' + req.body.subscription.endpoint)
+    }
+
+    res.send({
+      text: 'Web push subscribed',
+      status: '200'
+    })
+  } else if (req.body.action === 'unsubscribe') {
+    var subscriptionIndex = arrayObjectIndexOf(pushSubscriptions, req.body.subscription.endpoint, 'endpoint')
+
+    if (subscriptionIndex >= 0) {
+      pushSubscriptions.splice(subscriptionIndex, 1)
+
+      logger.info('Subscription unregistered: ' + req.body.subscription.endpoint)
+    } else {
+      logger.info('Subscription was not found: ' + req.body.subscription.endpoint)
+    }
+
+    res.send({
+      text: 'Web push unsubscribed',
+      status: '200'
+    })
+  } else {
+    throw new Error('Unsupported action')
+  }
+
+  logger.info('Number of active subscriptions: ' + pushSubscriptions.length)
+})
+
+// Listening to tweets stream and sending notifocation
 twitterClient.stream('statuses/filter', {
   track: stringToMonitor
 }, function (stream) {
@@ -80,7 +118,6 @@ twitterClient.stream('statuses/filter', {
         body: tweet.text,
         dir: 'auto',
         icon: tweet.user.profile_image_url_https,
-        image: tweet.user.profile_image_url_https,
         badge: tweet.user.profile_image_url_https,
         lang: tweet.lang,
         renotify: true,
@@ -90,31 +127,22 @@ twitterClient.stream('statuses/filter', {
         data: 'https://twitter.com/statuses/' + tweet.id_str
       }
 
-      sendNotification(JSON.stringify(notificationData))
+      if (tweet.entities && tweet.entities.media) {
+        notificationData.notification.image = tweet.entities.media[0].media_url_https
+      }
+
       logger.debug(notificationData)
       logger.debug(tweet)
       logger.info('Tweet stream received')
+
+      pushSubscriptions.forEach(function (item) {
+        sendNotification(item, JSON.stringify(notificationData))
+      })
     }
   })
 })
 
-// Subscribe to Web Push
-app.post('/webpush', function (req, res, next) {
-  if (req.body.action === 'subscribe') {
-    pushSubscription = req.body.subscription
-
-    logger.info('Subscription registered: ', pushSubscription)
-
-    res.send({
-      text: 'Subscribed',
-      status: '200'
-    })
-  } else {
-    throw new Error('Unsupported action')
-  }
-})
-
-function sendNotification (payload) {
+function sendNotification (pushSubscription, payload) {
   if (pushSubscription) {
     webPush.sendNotification(pushSubscription, payload)
       .then(function (response) {
@@ -128,8 +156,9 @@ function sendNotification (payload) {
   }
 }
 
-// Exposing the timeline endpoint
 // Go to https://dev.twitter.com/rest/tools/console to get endpoints list
+
+// Exposing the timeline endpoint
 app.get('/timeline/:screenName?', function (req, res, next) {
   twitterClient.get('statuses/user_timeline', {
     screen_name: req.params.screenName
@@ -143,7 +172,21 @@ app.get('/timeline/:screenName?', function (req, res, next) {
     })
 })
 
-// Subscribe to Web Push
+// Exposing the favorites endpoint
+app.get('/favorites/:screenName?', function (req, res, next) {
+  twitterClient.get('favorites/list', {
+    screen_name: req.params.screenName
+  })
+    .then(function (tweets) {
+      res.send(tweets)
+    })
+    .catch(function (error) {
+      logger.error(error)
+      throw new Error('Error receiving tweets')
+    })
+})
+
+// Placeholder to test Background Sync
 app.post('/post-tweet', function (req, res, next) {
   if (req.body.message) {
     logger.info('The data was received from front-end', req.body.message)
@@ -151,8 +194,16 @@ app.post('/post-tweet', function (req, res, next) {
       text: req.body.message,
       status: '200'
     })
+  } else {
+    throw new Error('Message text is required')
+  }
+})
 
-/*    twitterClient.post('statuses/update', {
+// Posting the tweet
+app.post('/real-post-tweet', function (req, res, next) {
+  if (req.body.message) {
+    logger.info('The data was received from front-end', req.body.message)
+    twitterClient.post('statuses/update', {
       status: req.body.message
     })
       .then(function (tweet) {
@@ -161,15 +212,15 @@ app.post('/post-tweet', function (req, res, next) {
       .catch(function (error) {
         logger.error(error)
         throw new Error('Error posting tweet')
-      })*/
+      })
   } else {
-    throw new Error('Unsupported action')
+    throw new Error('Message text is required')
   }
 })
 
 // Default endpoint
 app.get('/', function (req, res, next) {
-  res.send('PWA Workshop API works!')
+  res.send('PWA Workshop API works! Source: <a href="https://github.com/webmaxru/pwa-workshop-api">https://github.com/webmaxru/pwa-workshop-api</a>')
 })
 
 // Starting Express
@@ -187,20 +238,28 @@ new CronJob('*/5 * * * * *', function () {
   notificationData.notification = {
     actions: [{
       action: 'opentweet',
-      title: 'Open tweet'
+      title: 'Open root URL'
     }],
-    title: 'My title',
-    body: 'My notification',
+    title: 'Notification title',
+    body: 'This is Cron-basednotification for PWA-workshop',
     dir: 'auto',
-    icon: 'https://image',
+    icon: '/assets/images/logo.png',
     lang: 'en',
     renotify: true,
     requireInteraction: true,
     tag: 'Some ID',
     vibrate: [300, 100, 400],
-    data: 'Test data'
+    data: '/'
   }
 
   sendNotification(JSON.stringify(notificationData))
   logger.info(notificationData)
 }, null, false) // Set the last parameter to true to start CronJob
+
+// Utility function to search the item in the array of objects
+function arrayObjectIndexOf (myArray, searchTerm, property) {
+  for (var i = 0, len = myArray.length; i < len; i++) {
+    if (myArray[i][property] === searchTerm) return i
+  }
+  return -1
+}
